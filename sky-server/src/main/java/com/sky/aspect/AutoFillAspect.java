713e59ee987f82058f2d29ffa0da9f0126debb73
loseheart <871839107@qq.com>
2026-06-12 19:07:36 +0800

    feat(common): 添加自动填充功能处理公共字段
    - 创建AutoFill注解用于标识需要进行公共字段填充的方法
    - 实现AutoFillAspect切面类自动处理创建时间和更新时间字段
    - 自动填充创建人和更新人ID到实体对象
    - 在CategoryMapper中应用AutoFill注解替代手动赋值
    - 在EmployeeMapper中应用AutoFill注解处理员工信息更新
    - 移除CategoryServiceImpl和EmployeeServiceImpl中的手动字段赋值代码
    - 统一通过AOP方式处理所有实体的公共字段填充逻辑

---
+++ sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
@@ -0,0 +1,74 @@
+package com.sky.aspect;
+
+import com.sky.annotation.AutoFill;
+import com.sky.constant.AutoFillConstant;
+import com.sky.enumeration.OperationType;
+import com.sky.utils.UserContextUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.time.LocalDateTime;
+
+@Slf4j
+@Aspect
+@Component
+public class AutoFillAspect {
+
+    /*
+    * 切入点
+    * */
+    @Pointcut("execution(* com.sky.mapper.*.*(..)) && " +
+            "@annotation(com.sky.annotation.AutoFill)")
+    public void autoFillPointCut(){}
+
+    /*
+    * 前置通知，进行公共字段赋值
+    * */
+    @Before("autoFillPointCut()")
+    public void autoFill(JoinPoint joinPoint) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        log.info("开始进行公共字段填充");
+        //1.获取当前方法注解上的数据库操作类型
+
+        //1.1获取方法签名
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        //1.2获取方法上的注解对象
+        AutoFill autoFill = methodSignature.getMethod().getAnnotation(AutoFill.class);
+        //1.3获取数据库操作类型
+        OperationType operationType = autoFill.value();
+        //获取方法参数
+        Object[] args = joinPoint.getArgs();
+        if(args == null || args.length == 0){
+            return;
+        }
+        Object object = args[0];
+
+        //准备赋值数据
+        LocalDateTime now = LocalDateTime.now();
+        Long currentId = UserContextUtil.getCurrentEmployeeId();
+        //根据对应的数据库操作类型，为对应的字段赋值
+        if(operationType == OperationType.INSERT){
+            //获取实体对象的set方法
+            Method setCreateTime = object.getClass().getMethod(AutoFillConstant.SET_CREATE_TIME, LocalDateTime.class);
+            Method setUpdateTime = object.getClass().getMethod(AutoFillConstant.SET_UPDATE_TIME, LocalDateTime.class);
+            Method setCreateUser = object.getClass().getMethod(AutoFillConstant.SET_CREATE_USER, Long.class);
+            Method setUpdateUser = object.getClass().getMethod(AutoFillConstant.SET_UPDATE_USER, Long.class);
+            //调用方法，为对应的字段赋值
+            setCreateTime.invoke(object, now);
+            setUpdateTime.invoke(object, now);
+            setCreateUser.invoke(object, currentId);
+            setUpdateUser.invoke(object, currentId);
+        } else if (operationType == OperationType.UPDATE) {
+            Method setUpdateTime = object.getClass().getMethod(AutoFillConstant.SET_UPDATE_TIME, LocalDateTime.class);
+            Method setUpdateUser = object.getClass().getMethod(AutoFillConstant.SET_UPDATE_USER, Long.class);
+            setUpdateTime.invoke(object, now);
+            setUpdateUser.invoke(object, currentId);
+        }
+    }
+}
--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,8 +1,10 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
 import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.enumeration.OperationType;
 import com.sky.vo.EmployeeVO;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -25,6 +27,7 @@ public interface EmployeeMapper {
      */
     @Insert("insert into employee (username, name, password, phone, sex, id_number, create_time, update_time, create_user, update_user, status) " +
             "values (#{username}, #{name}, #{password}, #{phone}, #{sex}, #{idNumber}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser}, #{status})")
+    @AutoFill(value = OperationType.INSERT)
     void insert(Employee employee);
 
     /**
@@ -38,6 +41,7 @@ public interface EmployeeMapper {
      * 修改员工信息
      * @param employee 员工对象
      */
+    @AutoFill(value = OperationType.UPDATE)
     void update(Employee employee);
 
     /**
