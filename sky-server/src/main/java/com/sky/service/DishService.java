73ab16decbd94630d54040a146623e2487cea541
loseheart <871839107@qq.com>
2026-06-14 00:03:37 +0800

    feat(dish): 添加菜品管理分页查询功能
    - 在DishController中新增分页查询接口 /page
    - 在DishMapper中添加pageQuery方法和对应的SQL映射
    - 在DishService和DishServiceImpl中实现分页查询逻辑
    - 集成PageHelper实现分页功能
    - 添加DishPageQueryDTO和PageResult支持分页参数传递

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 
 public interface DishService {
 
@@ -9,4 +11,11 @@ public interface DishService {
      * @param dishDTO 菜品数据
      */
     void saveWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 菜品分页查询
+      * @param dishPageQueryDTO 分页查询参数
+      * @return PageResult
+     */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
