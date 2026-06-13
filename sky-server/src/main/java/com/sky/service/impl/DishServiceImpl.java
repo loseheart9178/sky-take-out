2b5b69ab2895a6ebf00881d8655c592dc45a0b1e
loseheart <871839107@qq.com>
2026-06-13 20:44:02 +0800

    feat(dish): 实现新增菜品功能
    - 新增菜品控制器DishController，提供新增菜品接口
    - 新增菜品口味映射器DishFlavorMapper及XML配置文件
    - 扩展菜品映射器DishMapper，添加菜品插入方法
    - 新增菜品服务接口DishService及实现类DishServiceImpl
    - 实现菜品与口味的批量保存功能，支持事务处理

---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -0,0 +1,51 @@
+package com.sky.service.impl;
+
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
+import com.sky.mapper.DishMapper;
+import com.sky.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class DishServiceImpl implements DishService {
+
+    @Autowired
+    private DishMapper dishMapper;
+    @Autowired
+    private DishFlavorMapper dishFlavorMapper;
+
+    /**
+     * 新增菜品，同时保存对应的口味数据
+     * @param dishDTO 菜品和口味数据
+     */
+    @Override
+    @Transactional
+    public void saveWithFlavor(DishDTO dishDTO) {
+        Dish dish = new Dish();
+        // 将dishDTO中的属性复制到dish对象中
+        BeanUtils.copyProperties(dishDTO, dish);
+        // 向菜品表插入1条数据
+        dishMapper.insert(dish);
+
+        //向菜品口味表插入n条数据
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        //获取insert返回的菜品id
+        Long dishId = dish.getId();
+        if(flavors != null && !flavors.isEmpty()){
+            //遍历flavors列表，设置每个口味的dishId
+            for (DishFlavor flavor : flavors) {
+                flavor.setDishId(dishId);
+            }
+            dishFlavorMapper.insertBatch(flavors);
+        }
+    }
+}
